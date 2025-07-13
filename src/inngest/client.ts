a877718798562c7c9bfee7bf0b4c1eed9adcc0cb
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-07-13 21:39:19 +0530

    fix: production fix

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,11 +1,9 @@
 import { Inngest } from 'inngest';
 import config from '../config/config.js';
 
-const isProd = config.nodeEnv === 'production';
-
 export const inngest = new Inngest({
   id: 'web-inspecto',
-  ...(isProd
-    ? { eventKey: config.inngestEventKey } 
-    : { baseUrl: config.inngestApiUrl }),
+  ...(config.nodeEnv !== 'production'
+    ? { baseUrl: config.inngestApiUrl }
+    : { eventKey: config.inngestEventKey }),
 });
