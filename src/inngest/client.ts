2f1575c34fb2d8ae52815bd1bcdcda6ab672e108
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-07-13 15:16:53 +0530

    fix: url fix

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -4,5 +4,5 @@ import config from '../config/config.js';
 export const inngest = new Inngest({
   id: 'web-inspecto',
   eventKey: config.inngestEventKey,
-  baseUrl: 'http://host.docker.internal:8288/v1',
+  baseUrl: config.inngestApiUrl,
 });
